ee9d3e36bfb97c1770b3e742007c50e6f09b3a7b
khl3ej <khl3ej@virginia.edu>
2018-02-25 22:25:11 -0500

    removed an incomplete line from reqs.js file

--- js/reqs.js
+++ js/reqs.js
@@ -5,6 +5,5 @@ function addItem(clickedButton){
   // no current work around this for setting button css attributes
   button.setAttribute("style", "background-color: #C492B1; border-radius: .25rem; margin-bottom: 10px; text-align: center; font-weight: 400; border: 1px solid transparent; padding: .375rem .75rem;");
   button.innerHTML = clickedButton;
-  if(ol)
   ol.appendChild(button);
 }
